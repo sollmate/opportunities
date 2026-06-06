3d06af54f9ccaa9410caf768c8ee345277e89d7f
Mikołaj Romek | Daidalus <mikolaj.romek@daidalus.eu>
2026-06-06 15:27:44 +0200

    Added new login ui

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -10,5 +10,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|login).*)"],
+  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|login|.*\\..*).*)"],
 };
